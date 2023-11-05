8a1916497c9f99966430a3363025d6a3ae18c624
SaiSirishaR <38254544+SaiSirishaR@users.noreply.github.com>
2023-11-05 20:13:16 +0100

    Update main.py

--- LSTM_TwitterSentimentClassifier/main.py
+++ LSTM_TwitterSentimentClassifier/main.py
@@ -36,36 +36,7 @@ class SentimentDataset(Dataset):
           return self.x[idx], self.y[idx]
         
         
-## Pre-processing steps
-
-class Preprocessing:
-      
-      def __init__(self, max_len, max_num_words):  
-        
-          self.data = 'data/tweets.csv'
-          self.max_num_words = max_num_words
-          self.max_len = max_len
-            
-      def load_data(self):
-        
-          data_inp = pd.read_csv(self.data)
-          data_inp = data_inp.drop(['location','keyword', 'id'], axis = 1)     
-          text_x = data_inp['text'].values         
-          labels_y = data_inp['target'].values            
-          self.train_text_x, self.test_text_x, self.train_labels_y, self.test_labels_y = train_test_split(text_x, labels_y, test_size=0.33)
-          print("train size", np.shape(self.train_text_x), "test shape", np.shape(self.test_text_x))  
-   
-      def prepare_tokens(self):
-          self.tokeniser = Tokenizer(num_words = self.max_num_words)
-          self.fit_data = self.tokeniser.fit_on_texts(self.train_text_x)
-            
-      def sequence_to_token(self, xtexts): 
-          sequences = self.tokeniser.texts_to_sequences(xtexts)
-          return sequence.pad_sequences(sequences, maxlen=self.max_len)
-        
-
 ### Calculating the accuracy
-
 
 def calculate_accuray(grand_truth, predictions):
     true_positives = 0
